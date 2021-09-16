9da1d704003ff21ac4a4fd8c0afc1f1e053b1de0
Andrey Laktyushin <andrey@laktyushin.com>
2021-09-16 18:50:08 +0300

    1. Читаем файл конфигурации [#858 #203574]

--- src/main/java/ru/job4j/io/Config.java
+++ src/main/java/ru/job4j/io/Config.java
@@ -4,37 +4,35 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 public class Config {
 
     private final String path;
-    private final Map<String, String> values = new HashMap<String, String>();
+    private final Map<String, String> values = new HashMap<>();
 
     public Config(final String path) {
         this.path = path;
     }
 
     public void load() {
-        List<String> stringList = null;
         try (BufferedReader read = new BufferedReader(new FileReader(this.path))) {
-            stringList = read
+            values.putAll(read
                     .lines()
                     .filter(l -> !l.contains("#") && !l.equals(""))
-                    .collect(Collectors.toList());
+                    .collect(Collectors.toMap(
+                            k -> {
+                                if (k.split("=").length < 2) {
+                                    throw new IllegalArgumentException("Missing key or value!");
+                                }
+                                return k.split("=")[0];
+                            },
+                            v -> v.split("=")[1]))
+            );
         } catch (IOException e) {
             System.out.println("File not found: " + path);
             //e.printStackTrace();
-        }
-        if (stringList != null) {
-            for (var v : stringList) {
-                String[] pair = v.split("=");
-                if (pair.length > 1) {
-                    values.put(pair[0], pair[1]);
-                } else throw new IllegalArgumentException("Missing key or value in string: " + v);
-            }
         }
     }
 
