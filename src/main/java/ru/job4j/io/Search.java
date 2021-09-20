6d0435f483b21b9ff906934c1599fb087fb7aea7
Andrey Laktyushin <andrey@laktyushin.com>
2021-09-20 15:35:20 +0300

    4.1. Сканирование файловой системы. [#106929 #204785]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -10,13 +10,12 @@ import java.util.function.Predicate;
 public class Search {
     public static void main(String[] args) throws IOException {
         Path start = Paths.get(".");
-        search(start, p -> p.toFile().getName().endsWith(".java")).forEach(System.out::println);
+        search(start, p -> p.toFile().getName().endsWith(".txt")).forEach(System.out::println);
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
         SearchFiles searcher = new SearchFiles(condition);
         Files.walkFileTree(root, searcher);
-        searcher.setPath(root);
         return searcher.getPaths();
     }
 }
--- src/main/java/ru/job4j/io/SearchFiles.java
+++ src/main/java/ru/job4j/io/SearchFiles.java
@@ -2,25 +2,48 @@ package ru.job4j.io;
 
 import java.io.IOException;
 import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
-public class SearchFiles extends SimpleFileVisitor<Path> {
+import static java.nio.file.FileVisitResult.CONTINUE;
+
+public class SearchFiles implements FileVisitor<Path> {
 
     Predicate<Path> condition;
-    Path path;
+    List<Path> paths = new LinkedList<>();
 
     public SearchFiles(Predicate<Path> condition) {
         this.condition = condition;
     }
 
-    public List<Path> getPaths() throws IOException {
-        return Files.walk(path)
-                    .filter(condition)
-                    .toList();
+    public List<Path> getPaths() {
+        return paths
+                .stream()
+                .filter(condition)
+                .collect(Collectors.toList());
     }
 
-    public void setPath(Path path) {
-        this.path = path;
+    @Override
+    public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) {
+        return CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
+        paths.add(file.toAbsolutePath());
+        return CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult visitFileFailed(Path file, IOException exc) {
+        return CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult postVisitDirectory(Path dir, IOException exc) {
+        return CONTINUE;
     }
 }
