8c108f39408e14f7ec9cd4335715e9766563a856
Andrey Laktyushin <andrey@laktyushin.com>
2021-09-20 16:10:40 +0300

    4.2. Поиск дубликатов [#315066]

--- src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
@@ -5,27 +5,25 @@ import java.nio.file.FileVisitResult;
 import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
 
 public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
-    Map<String, List<Path>> fileMap = new HashMap<>();
+
+    Map<FileProperty, Path> map = new HashMap<>();
 
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-        String str = file.getFileName().toString() + " " + file.toFile().length();
-        //System.out.println(str);
-        //System.out.println(file.toAbsolutePath());
-        //System.out.println(file.getFileName().toString() + "\t" + file.toFile().length() + "\t" + file.toAbsolutePath());
-        //fileMap.put(new FileProperty(file.toFile().length(), file.getFileName().toString()), file.toAbsolutePath());
-        List<Path> paths = new LinkedList<>();
-        paths.add(file.toAbsolutePath());
-        fileMap.put(str, paths);
-        fileMap.keySet().forEach(System.out::println);
-
+        FileProperty fileProperty = new FileProperty(file.toFile().length(), file.getFileName().toString());
+        Optional<Path> rsl = Optional.ofNullable(map.put(fileProperty, file.toAbsolutePath()));
+        if (rsl.isPresent()) {
+            System.out.println("Duplicate is found (name/size):");
+            System.out.println(fileProperty.getName() + "\t" + fileProperty.getSize());
+            System.out.println(file.toAbsolutePath());
+            System.out.println(rsl.get());
+            System.out.println();
+        }
         return super.visitFile(file, attrs);
-    }
-
-    public Map<String, List<Path>> getFileMap() {
-        return fileMap;
     }
 }
