106ede0ac31b5e09a61450f20e095aa3601ff7f2
Andrey Laktyushin <andrey@laktyushin.com>
2021-09-22 16:17:05 +0300

    5.1. Именованные аргументы [#295518]

---
+++ src/main/java/ru/job4j/io/ArgsName.java
@@ -0,0 +1,39 @@
+package ru.job4j.io;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ArgsName {
+
+    private final Map<String, String> values = new HashMap<>();
+
+    public String get(String key) {
+        return values.get(key);
+    }
+
+    private void parse(String[] args) {
+        if (args.length == 0) {
+            throw new IllegalArgumentException("Missing arguments.");
+        }
+        for (var a : args) {
+            if (!a.startsWith("-") || a.split("=").length < 2) {
+                throw new IllegalArgumentException("Incorrect argument(s) format: must be -ARG=VALUE");
+            }
+            values.put(a.split("=")[0].split("-")[1], a.split("=")[1]);
+        }
+    }
+
+    public static ArgsName of(String[] args) {
+        ArgsName names = new ArgsName();
+        names.parse(args);
+        return names;
+    }
+
+    public static void main(String[] args) {
+        ArgsName jvm = ArgsName.of(new String[] {"-Xmx=512", "-encoding=UTF-8"});
+        System.out.println(jvm.get("Xmx"));
+
+        ArgsName zip = ArgsName.of(new String[] {"-out=project.zip", "-encoding=UTF-8"});
+        System.out.println(zip.get("out"));
+    }
+}
