e56563269d49d276410888e8a3cc62fac6aac343
Andrey Laktyushin <andrey@laktyushin.com>
2021-09-22 15:21:06 +0300

    5. Валидация параметров запуска. [#246865]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -1,5 +1,6 @@
 package ru.job4j.io;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -9,8 +10,13 @@ import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, p -> p.toFile().getName().endsWith(".txt")).forEach(System.out::println);
+        if (args.length < 2) {
+            throw new IllegalArgumentException("Missing arguments. Usage: java -jar dir.jar ROOT_FOLDER EXTENSION.");
+        }
+        Path start = Path.of(args[0]);
+        String extension = args[1];
+        System.out.println("Start dir: " + args[0] + "\t File ends with: " + args[1]);
+        search(start, p -> p.toFile().getName().endsWith(extension)).forEach(System.out::println);
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
