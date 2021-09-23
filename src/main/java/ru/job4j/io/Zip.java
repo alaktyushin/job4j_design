0309c9d64bd832f8a1c67c18676fd5ac375aac32
Andrey Laktyushin <andrey@laktyushin.com>
2021-09-23 16:05:17 +0300

    5.2. Архивировать проект [#861 #206931]

--- src/main/java/ru/job4j/io/Zip.java
+++ src/main/java/ru/job4j/io/Zip.java
@@ -10,6 +10,9 @@ import java.util.zip.ZipOutputStream;
 import static ru.job4j.io.Search.search;
 
 public class Zip {
+    private static String extension;
+    private static Path target;
+    private static List<File> list;
 
     public static void packFiles(List<File> sources, File target) {
         try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
@@ -35,25 +38,29 @@ public class Zip {
         }
     }
 
-    public static void main(String[] args) throws IOException {
+    private static void validateArgs(String[] args) throws IllegalArgumentException, IOException {
         if (args.length != 3) {
             throw new IllegalArgumentException("Missing arguments.");
         }
         ArgsName argsName = ArgsName.of(args);
         Path path = Path.of(argsName.get("d"));
         if (!Files.exists(path)) {
-            throw new FileNotFoundException("Source directory is not found: " + path);
+            throw new IllegalArgumentException("Source directory is not found: " + path);
         }
-        String extension = argsName.get("e");
-        Path target = Path.of(argsName.get("o"));
-        System.out.println("Source directory: " + path);
-        System.out.println("Extension to exclude: " + argsName.get("e"));
-        System.out.println("Target file: " + argsName.get("o"));
-        List<File> list = search(path, p -> !p.toFile().getName().endsWith(extension))
+        extension = argsName.get("e");
+        target = Path.of(argsName.get("o"));
+        list = search(path, p -> !p.toFile().getName().endsWith(extension))
                 .stream()
                 .map(Path::toFile)
                 .toList();
+        System.out.println("Source directory: " + path);
+        System.out.println("Extension to exclude: " + argsName.get("e"));
+        System.out.println("Target file: " + argsName.get("o"));
         System.out.println("Packing...");
+    }
+
+    public static void main(String[] args) throws IOException {
+        validateArgs(args);
         packFiles(list, new File(args[2].split("=")[1]));
         if (Files.exists(target) && target.toFile().length() > 0) {
             System.out.println("Packed successfully: " + target + "\t" + target.toFile().length() + " bytes");
