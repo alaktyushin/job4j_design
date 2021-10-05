47f297b41b59434b0af5e9c6f241bbdd1dbb3676
Andrey Laktyushin <andrey@laktyushin.com>
2021-10-05 10:25:10 +0300

    1. Log4j. Логирование системы. [#95335 #212077]

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -1,32 +1,17 @@
 package ru.job4j.io;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 
 public class UsageLog4j {
 
-    private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
+    private static final Logger LOG = LogManager.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        String name = "Petr Arsentev";
-        int age = 33;
-        LOG.debug("User info name : {}, age : {}\n", name, age);
-        boolean bool = true;
-        char c = 'A';
-        byte b = 1;
-        short s = 17;
-        int i = 30_000;
-        long l = 28L;
-        float f = 3.14f;
-        double d = 3.14;
-        LOG.info("Primitive variables:");
-        LOG.info("boolean {}", bool);
-        LOG.info("char {}", c);
-        LOG.info("byte {}", b);
-        LOG.info("short {}", s);
-        LOG.info("int {}", i);
-        LOG.info("long {}", l);
-        LOG.info("float {}", f);
-        LOG.info("double {}", d);
+        LOG.trace("trace message");
+        LOG.debug("debug message");
+        LOG.info("info message");
+        LOG.warn("warn message");
+        LOG.error("error message");
     }
 }
