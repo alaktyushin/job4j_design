b4962020640930354758a1d4791dcfe1a2ed36ba
Andrey Laktyushin <andrey@laktyushin.com>
2021-10-10 18:39:35 +0300

    2. Поиск файлов по критерию [#783 #214051]

--- src/main/java/ru/job4j/find/ArgsName.java
+++ src/main/java/ru/job4j/find/ArgsName.java
@@ -7,25 +7,34 @@ public class ArgsName {
 
     private final Map<String, String> values = new HashMap<>();
 
-    public String get(String key) {
+    String get(String key) {
         return values.get(key);
     }
 
     private void parse(String[] args) {
         if (args.length < 2) {
             throw new IllegalArgumentException(
-                    "Missing arguments. Usage: -d=PATH -n=CONDITION(optional) -t=MASK(optional) -o=FILENAME"
+                    "Missing arguments. Usage: -d=PATH -n=CONDITION(optional) -t=MASK(optional) -o=LOG_FILENAME"
             );
         }
         for (var a : args) {
-            if (!a.startsWith("-") || a.split("=").length < 2) {
+            if (!a.startsWith("-") || a.split("=").length != 2) {
                 throw new IllegalArgumentException("Incorrect argument(s) format: must be -ARG=VALUE");
             }
             values.put(a.split("=")[0].split("-")[1], a.split("=")[1]);
         }
+        if (values.get("d") == null) {
+            throw new IllegalArgumentException("Missing -d value (PATH).");
+        }
+        if (values.get("o") == null) {
+            throw new IllegalArgumentException("Missing -o value (LOG_FILENAME).");
+        }
+        if (!values.get("t").equals("name") && !values.get("t").equals("mask") && !values.get("t").equals("regex")) {
+            System.out.println("Search type doesn't match \"name\", \"mask\" or \"regex\". All files will be shown.");
+        }
     }
 
-    public static ArgsName of(String[] args) {
+    static ArgsName of(String[] args) {
         ArgsName names = new ArgsName();
         names.parse(args);
         return names;
--- src/main/java/ru/job4j/find/FindFiles.java
+++ src/main/java/ru/job4j/find/FindFiles.java
@@ -9,21 +9,21 @@ import java.nio.file.Path;
 import java.util.Date;
 import java.util.List;
 import java.util.function.Predicate;
+import java.util.regex.Pattern;
 import java.util.regex.PatternSyntaxException;
 
 public class FindFiles {
 
-    private static String logPath;
-    private static Predicate<Path> condition;
+    private String logPath;
+    private Predicate<Path> condition;
 
-    private static final boolean LOG_APPEND = true;
-    private static final String LOG_HEAD =
-            System.lineSeparator()
-                    + "Search started on: "
-                    + new Date()
-                    + System.lineSeparator();
-
-    private static void saveLog(List<Path> log) {
+    private void saveLog(List<Path> log) {
+        final boolean LOG_APPEND = false;
+        final String LOG_HEAD =
+                System.lineSeparator()
+                        + "Search started on: "
+                        + new Date()
+                        + System.lineSeparator();
         try (PrintWriter pw = new PrintWriter(
                 new FileWriter(logPath, StandardCharsets.UTF_8, LOG_APPEND))) {
             pw.print(LOG_HEAD);
@@ -33,23 +33,32 @@ public class FindFiles {
         }
     }
 
-    private static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
+    private List<Path> search(Path root, Predicate<Path> condition) throws IOException {
         SearchFiles searcher = new SearchFiles(condition);
         Files.walkFileTree(root, searcher);
         return searcher.getPaths();
     }
 
-    private static void setCondition(String searchType, String searchArg) throws PatternSyntaxException {
+    private void setCondition(String searchType, String searchArg) throws PatternSyntaxException {
         condition =
             switch (searchType) {
                 case "name" -> p -> p.toFile().getName().equals(searchArg);
-                case "mask" -> p -> p.toFile().getName().contains(searchArg);
-                case "regex" -> p -> p.toFile().getName().matches(searchArg);
+                case "mask" -> p -> Pattern
+                        .compile(searchArg
+                                .replace(".", "[.]")
+                                .replace("*", ".*")
+                                .replace("?", "."))
+                        .matcher(p.toFile().getName())
+                        .matches();
+                case "regex" -> p -> Pattern
+                        .compile(searchArg)
+                        .matcher(p.toFile().getName())
+                        .matches();
                 default -> p -> true;
             };
     }
 
-    public static void handle(ArgsName argsName) throws IOException {
+    private void handle(ArgsName argsName) throws IOException {
         Path start = Path.of(argsName.get("d")).normalize();
         logPath = argsName.get("o");
         setCondition(argsName.get("t"), argsName.get("n"));
@@ -57,7 +66,7 @@ public class FindFiles {
     }
 
     public static void main(String[] args) throws IOException {
-        ArgsName jvm = ArgsName.of(args);
-        handle(jvm);
+        FindFiles findFiles = new FindFiles();
+        findFiles.handle(ArgsName.of(args));
     }
 }
