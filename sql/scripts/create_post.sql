1a4b1a88718079c1f4bbab9ab66c2ba9b9991969
Andrey Laktyushin <andrey@laktyushin.com>
2021-10-30 13:38:56 +0300

    4. Схема таблицы Post [#1731 #220398]

--- sql/scripts/create_post.sql
+++ sql/scripts/create_post.sql
@@ -1,9 +1,8 @@
 CREATE TABLE post
 (
-    id integer NOT NULL,
+    id integer NOT NULL CONSTRAINT post_pkey PRIMARY KEY,
     name varchar (250),
 	text varchar (30000),
-	link varchar (250),
-	created timestamp,
-    CONSTRAINT post_pkey PRIMARY KEY (id)
+	link varchar (250) CONSTRAINT post_link UNIQUE,
+	created timestamp
 );
