fd6660c2c74eea6f8bc4132ada3f716d52ecf4c2
Andrey N Laktyushin <andrey@laktyushin.com>
2022-05-30 16:30:31 +0300

    1. Принципы Kiss, Dry и Yagni [#238813]

---
+++ src/main/java/ru/job4j/kiss/MaxMin.java
@@ -0,0 +1,27 @@
+package ru.job4j.kiss;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class MaxMin {
+        public <T> T max(List<T> value, Comparator<T> comparator) {
+            return compareList(value, comparator);
+        }
+
+    public <T> T min(List<T> value, Comparator<T> comparator) {
+        return compareList(value, comparator.reversed());
+    }
+
+        public <T> T compareList(List<T> value, Comparator<T> comparator) {
+            T temp = null;
+            if (value.size() > 0) {
+                temp = value.get(0);
+                for (int i = 1; i < value.size(); i++) {
+                    if (comparator.compare(value.get(i), temp) > 0) {
+                        temp = value.get(i);
+                    }
+                }
+            }
+            return temp;
+        }
+}
---
+++ src/test/java/ru/job4j/kiss/MaxMinTest.java
@@ -0,0 +1,75 @@
+package ru.job4j.kiss;
+
+import org.junit.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+public class MaxMinTest {
+
+    @Test
+    public void maxWhenSizeIsNull() {
+        MaxMin maxMin = new MaxMin();
+        ArrayList<Integer> list = new ArrayList<>();
+        assertNull(maxMin.max(list, Integer::compareTo));
+    }
+
+    @Test
+    public void minWhenSizeIsNull() {
+        MaxMin maxMin = new MaxMin();
+        ArrayList<Integer> list = new ArrayList<>();
+        assertNull(maxMin.min(list, Integer::compareTo));
+    }
+
+    @Test
+    public void maxIntegerWhenSizeIsGreaterThenNull() {
+        MaxMin maxMin = new MaxMin();
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(2);
+        list.add(4);
+        list.add(-2);
+        assertThat(maxMin.max(list, Integer::compareTo), is (4));
+    }
+
+    @Test
+    public void maxStringWhenSizeIsGreaterThenNull() {
+        MaxMin maxMin = new MaxMin();
+        ArrayList<String> list = new ArrayList<>();
+        list.add("1");
+        list.add("2");
+        list.add("333");
+        list.add("a2");
+        assertThat(maxMin.max(list, String::compareTo), is ("a2"));
+    }
+
+    @Test
+    public void minWhenSizeIsGreaterThenNull() {
+        MaxMin maxMin = new MaxMin();
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(2);
+        list.add(4);
+        list.add(-2);
+        assertThat(maxMin.min(list, Integer::compareTo), is (-2));
+    }
+
+    @Test
+    public void minStringWhenSizeIsGreaterThenNull() {
+        MaxMin maxMin = new MaxMin();
+        ArrayList<String> list = new ArrayList<>();
+        list.add("1");
+        list.add("2");
+        list.add("333");
+        list.add("a2");
+        //assertThat(maxMin.max(list, (a, b) -> a.compareTo(b)), is ("1"));
+        assertThat(maxMin.min(list, String::compareTo), is ("1"));
+    }
+}
