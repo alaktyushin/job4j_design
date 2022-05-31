5f74e0721207ee3f59d0e7c621214030f5286eb1
Andrey N Laktyushin <andrey@laktyushin.com>
2022-05-31 14:27:56 +0300

    3. Шаблонизатор. [#855]

---
+++ src/test/java/ru/job4j/template/GeneratorTest.java
@@ -0,0 +1,53 @@
+package ru.job4j.template;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class GeneratorTest {
+
+    @Ignore
+    @Test(expected = IllegalArgumentException.class)
+    public void produceWhenValidTemplateAndMapIsEmpty() {
+        Generator generator = new TemplateGenerator();
+        String template = "I am a ${name}, Who are ${subject}?";
+        Map<String, String> map = new HashMap<>();
+        assertThat(generator.produce(template, map), is ("I am a Petr Arsentev, Who are you?"));
+    }
+
+    @Ignore
+    @Test
+    public void produceWhenValidTemplateAndValidMap() {
+        Generator generator = new TemplateGenerator();
+        String template = "I am a ${name}, Who are ${subject}?";
+        Map<String, String> map = new HashMap<>();
+        map.put("Petr Arsentev", "you");
+        assertThat(generator.produce(template, map), is ("I am a Petr Arsentev, Who are you?"));
+    }
+
+    @Ignore
+    @Test(expected = IllegalArgumentException.class)
+    public void produceWhenInvalidTemplate() {
+        Generator generator = new TemplateGenerator();
+        String template = "I am a ${name} ${family_name}, Who are ${subject}?";
+        Map<String, String> map = new HashMap<>();
+        map.put("Petr Arsentev", "you");
+        assertThat(generator.produce(template, map), is ("I am a Petr Arsentev, Who are you?"));
+    }
+
+    @Ignore
+    @Test(expected = IllegalArgumentException.class)
+    public void produceWhenValidTemplateAndInvalidMap() {
+        Generator generator = new TemplateGenerator();
+        String template = "I am a ${name}, Who are ${subject}?";
+        Map<String, String> map = new HashMap<>();
+        map.put("Petr Arsentev", "you");
+        map.put("Petr Arsentev", "your");
+        assertThat(generator.produce(template, map), is ("I am a Petr Arsentev, Who are you?"));
+    }
+}
